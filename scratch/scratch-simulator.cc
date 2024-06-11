81a0465a33dc39955334c53ce7c4f86ec60ce5ba
Jynns <jannisarnold1@gmail.com>
2024-06-11 16:47:45 +0200

    basic setup for connection

--- scratch/scratch-simulator.cc
+++ scratch/scratch-simulator.cc
@@ -14,7 +14,11 @@
  */
 
 #include "ns3/core-module.h"
+#include "ns3/internet-stack-helper.h"
+#include "ns3/ipv4-address-helper.h"
 #include "ns3/mp-tcp-socket-base.h"
+#include "ns3/node-container.h"
+#include "ns3/point-to-point-helper.h"
 
 using namespace ns3;
 
@@ -24,12 +28,41 @@ int
 main(int argc, char* argv[])
 {
     NS_LOG_UNCOND("Scratch Simulator");
-    
+
+    // Config::SetDefault("ns3::Ipv4GlobalRouting::FlowEcmpRouting", BooleanValue(true));
+    Config::SetDefault("ns3::TcpSocket::SegmentSize", UintegerValue(1400));
+    Config::SetDefault("ns3::TcpSocket::DelAckCount", UintegerValue(0));
+    // Config::SetDefault("ns3::DropTailQueue::Mode", StringValue("QUEUE_MODE_PACKETS"));
+    // Config::SetDefault("ns3::DropTailQueue::MaxPackets", UintegerValue(100));
+    Config::SetDefault("ns3::TcpL4Protocol::SocketType", TypeIdValue(MpTcpSocketBase::GetTypeId()));
+    Config::SetDefault("ns3::MpTcpSocketBase::MaxSubflows", UintegerValue(8));
+
     Simulator::Run();
     Simulator::Destroy();
-    Ptr<MpTcpSocketBase> mp = Create<MpTcpSocketBase>();
+    NodeContainer nodes;
+    nodes.Create(2);
 
-    
 
+    PointToPointHelper pointToPoint;
+    pointToPoint.SetDeviceAttribute("DataRate", StringValue("5Mbps"));
+    pointToPoint.SetChannelAttribute("Delay", StringValue("2ms"));
+    NetDeviceContainer devices = pointToPoint.Install(nodes);
+
+    InternetStackHelper internet;
+    internet.Install(nodes);
+
+    Ipv4AddressHelper ipv4;
+    ipv4.SetBase("10.1.1.0", "255.255.255.0");
+    Ipv4InterfaceContainer i = ipv4.Assign(devices);
+
+
+    TypeId tid = TypeId::LookupByName("ns3::TcpNewReno");
+    //Config::Set("/NodeList/*/$ns3::TcpL4Protocol/SocketType", TypeIdValue(tid));
+    //-> there has to be a valid ns3 congestion algo when creating the socke
+
+    Ptr<MpTcpSocketBase> mp1 = DynamicCast<MpTcpSocketBase>(Socket::CreateSocket (nodes.Get(0), TcpSocketFactory::GetTypeId ()));
+    std::cout << "awdw" << std::endl;
+    auto t = devices.Get(0);
+    mp1->BindToNetDevice(devices.Get(0));
     return 0;
 }
